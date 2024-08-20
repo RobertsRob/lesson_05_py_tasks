01852e5a58b37e0108d5712c4aef99ccf2d95bad
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2024-08-20 17:25:41 +0300

    Update tasks_2.py

--- tasks_2.py
+++ tasks_2.py
@@ -1,1 +1,28 @@
+# 1.taks
+# Напишите функцию, которая принимает целое число n и выводит на экран все числа от 1 до n (включительно).
+def print_numbers(n):
+    for i in range(1, n+1):
+        print(i)
+
+print_numbers(5)
+
+# 2.taks
+
+# 3.taks
+
+# 4.taks
+
+# 5.taks
+
+# 6.taks
+
+# 7.taks
+
+# 8.taks
+
+# 9.taks
+
+# 10.taks
+
+
 
